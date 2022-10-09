fd9c4c43d6be9c8cfee5cb55a4fad1d9330fec0f
neilcantorne <cantorne.neil@gmail.com>
2022-10-10 06:55:18 +0800

    initialize package

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dart xxHash Binding
+A Dart binding of [xxHash](https://github.com/Cyan4973/xxHash)
---
+++ test/dart_xxh_test.dart
@@ -0,0 +1,12 @@
+//import 'package:dart_xxh/dart_xxh.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {});
+  });
+}
