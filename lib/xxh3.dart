f53278c8c2fa908dff1861b9fc7ce4c2b12a382b
neilcantorne <cantorne.neil@gmail.com>
2022-10-10 21:20:51 +0800

    implement finalizer

--- lib/xxh3.dart
+++ lib/xxh3.dart
@@ -2,12 +2,16 @@ import 'dart:ffi';
 
 import 'package:dart_xxh/loader.dart';
 
-typedef FnCreateState = Pointer<NativeType> Function();
 typedef Xxh3State = Pointer<NativeType>;
+typedef FnCreateState = Xxh3State Function();
+typedef FnDestroyState = Void Function(Xxh3State);
 
 class Xxh3 {
   final dylib = loadLibrary();
   late Xxh3State state;
+
+  // ignore: unused_field
+  static final Finalizer<Xxh3> _finalizer = Finalizer((xxh3) => xxh3.dispose());
 
   Xxh3() {
     final FnCreateState createState = dylib
@@ -15,5 +19,13 @@ class Xxh3 {
         .asFunction();
 
     state = createState();
+  }
+
+  void dispose() {
+    final void Function(Xxh3State) destroyState = dylib
+        .lookup<NativeFunction<FnDestroyState>>("destroy_state")
+        .asFunction();
+
+    destroyState(state);
   }
 }
